542c8086220598892cea20d897c2098dc2ea8e23
Miko <MikoSecSoS@gmail.com>
2019-06-21 23:28:57 +0800

    first commit

---
+++ Lession1/src/fx/com/Main.java
@@ -0,0 +1,16 @@
+package fx.com;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("I Love Miko^_^");
+		primaryStage.show();
+	}
+}
