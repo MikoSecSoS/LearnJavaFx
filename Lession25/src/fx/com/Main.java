cfabeebba520f7f8f2daf2c94f80ef5be2e73de1
Miko <MikoSecSoS@gmail.com>
2019-06-29 23:53:27 +0800

    commit

--- Lession25/src/fx/com/Main.java
+++ Lession25/src/fx/com/Main.java
@@ -1,4 +1,56 @@
 package fx.com;
 
-public class Main {
+import javafx.application.Application;
+import javafx.application.HostServices;
+import javafx.concurrent.ScheduledService;
+import javafx.concurrent.Task;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        VBox vBox = new VBox();
+
+        Hyperlink hyperlink = new Hyperlink("http://www.baidu.com/", new Button("Baidu"));
+
+        hyperlink.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                HostServices hostServices = Main.this.getHostServices();
+
+                hostServices.showDocument(hyperlink.getText());
+            }
+        });
+
+        vBox.getChildren().add(hyperlink);
+
+        Scene scene = new Scene(vBox);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Miko is lovely AI?");
+        primaryStage.setWidth(700);
+        primaryStage.setHeight(550);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
 }
