5069aea0367cafa8f63d95b64b35cbed20aa73f8
Miko <MikoSecSoS@gmail.com>
2019-06-29 23:59:52 +0800

    commit

--- Lession25/src/fx/com/Main.java
+++ Lession25/src/fx/com/Main.java
@@ -2,24 +2,14 @@ package fx.com;
 
 import javafx.application.Application;
 import javafx.application.HostServices;
-import javafx.concurrent.ScheduledService;
-import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.DialogPane;
 import javafx.scene.control.Hyperlink;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-import javafx.util.Duration;
 
 public class Main extends Application {
 
@@ -32,7 +22,10 @@ public class Main extends Application {
 
         VBox vBox = new VBox();
 
-        Hyperlink hyperlink = new Hyperlink("http://www.baidu.com/", new Button("Baidu"));
+        Hyperlink hyperlink = new Hyperlink("http://www.baidu.com/", new Button("OpenLink"));
+
+        TextField textField = new TextField();
+        Button button = new Button("SetLink");
 
         hyperlink.setOnAction(new EventHandler<ActionEvent>() {
             @Override
@@ -43,7 +36,14 @@ public class Main extends Application {
             }
         });
 
-        vBox.getChildren().add(hyperlink);
+        button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                hyperlink.setText(textField.getText());
+            }
+        });
+
+        vBox.getChildren().addAll(hyperlink, textField, button);
 
         Scene scene = new Scene(vBox);
         primaryStage.setScene(scene);
