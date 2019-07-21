efe3de202ecea701484401f68e0ecde6b8a88428
Miko <MikoSecSoS@gmail.com>
2019-07-21 14:46:24 +0800

    commit

--- Lession40/src/fx/com/Main.java
+++ Lession40/src/fx/com/Main.java
@@ -1,17 +1,19 @@
 package fx.com;
 
 import javafx.application.Application;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
-import javafx.util.Callback;
 import javafx.util.StringConverter;
 
 public class Main extends Application {
@@ -23,42 +25,48 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
 
+        Student student0 = new Student("Miko", 16, 0);
+        Student student1 = new Student("Code", 16, 100);
+        Student student2 = new Student("Cute", 14, 90);
+        Student student3 = new Student("AI", 3, 999.999);
+
+        TextField nameTF = new TextField();
+
+        Button modifyNameBut = new Button("Modify Name");
 
         AnchorPane anchorPane = new AnchorPane();
         anchorPane.setStyle("-fx-background-color: #EECFA1");
 
-        ComboBox<String> comboBox = new ComboBox<String>();
-        comboBox.getItems().addAll("Cute - 00", "Cute - 01", "Cute - 02");
+        ChoiceBox<Student> choiceBox = new ChoiceBox<Student>();
+        choiceBox.getItems().addAll(student0, student1, student2, student3);
 
-        comboBox.setConverter(new StringConverter<String>() {
+        choiceBox.getSelectionModel().select(1);
+
+        choiceBox.setConverter(new StringConverter<Student>() {
             @Override
-            public String toString(String object) {
-//                System.out.println(object);
-                return object;
+            public String toString(Student object) {
+                String value = object.getName() + " - " + object.getAge() + " - " + object.getScore();
+                return value;
             }
 
             @Override
-            public String fromString(String string) {
+            public Student fromString(String string) {
                 return null;
             }
         });
 
-        comboBox.setCellFactory(new Callback<ListView<String>, ListCell<String>>() {
-            @Override
-            public ListCell<String> call(ListView<String> param) {
-                My_ListCell<String> listCell = new My_ListCell<String>();
-//                param.getItems().forEach(System.out::println);
-//                param.getItems().forEach(item -> System.out.println("[Data] " + item));
-//                param.getItems().forEach(item -> listCell.setText(item));
-                return listCell;
-            }
-        });
+        choiceBox.setPrefWidth(200);
 
-        comboBox.setPrefWidth(150);
-        AnchorPane.setTopAnchor(comboBox, 100.0);
-        AnchorPane.setLeftAnchor(comboBox, 10.0);
+        AnchorPane.setTopAnchor(choiceBox, 100.0);
+        AnchorPane.setLeftAnchor(choiceBox, 10.0);
 
-        anchorPane.getChildren().addAll(comboBox);
+        AnchorPane.setTopAnchor(nameTF, 50.0);
+        AnchorPane.setLeftAnchor(nameTF, 10.0);
+
+        AnchorPane.setTopAnchor(modifyNameBut, 50.0);
+        AnchorPane.setLeftAnchor(modifyNameBut, 200.0);
+
+        anchorPane.getChildren().addAll(choiceBox,  nameTF, modifyNameBut);
 
         Scene scene = new Scene(anchorPane);
         primaryStage.setScene(scene);
@@ -67,31 +75,104 @@ public class Main extends Application {
         primaryStage.setHeight(650);
         primaryStage.show();
 
+        choiceBox.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Student>() {
+            @Override
+            public void changed(ObservableValue<? extends Student> observable, Student oldValue, Student newValue) {
+//                System.out.println("[Name] " + newValue.getName());
+//                System.out.println("[Age] " + newValue.getAge());
+//                System.out.println("[Score] " + newValue.getScore());
+            }
+        });
+
+        modifyNameBut.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+//                Student student = choiceBox.getSelectionModel().getSelectedItem();
+//
+//                int i = choiceBox.getItems().indexOf(student);
+//
+//                choiceBox.getItems().remove(i);
+//
+//                student.setName(nameTF.getText());
+//
+//                choiceBox.getItems().add(i, student);
+
+                choiceBox.getSelectionModel().getSelectedItem().setName(nameTF.getText());
+
+//                choiceBox.getItems().forEach(s -> System.out.println(s.getName()));
+
+            }
+        });
+
+        choiceBox.getSelectionModel().getSelectedItem().getNameSimpleStringProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                Student student = choiceBox.getSelectionModel().getSelectedItem();
+
+                int i = choiceBox.getItems().indexOf(student);
+
+                choiceBox.getItems().remove(i);
+
+//                student.setName(nameTF.getText());
+
+                choiceBox.getItems().add(i, student);
+            }
+        });
+
     }
 }
-class My_ListCell<T> extends ListCell<String> {
-    @Override
-    protected void updateItem(String item, boolean empty) {
-        super.updateItem(item, empty);
 
-        if (empty) {
-            return;
-        }
+class Student {
+    private SimpleStringProperty name = new SimpleStringProperty();
+    private SimpleIntegerProperty age = new SimpleIntegerProperty();
+    private SimpleDoubleProperty score = new SimpleDoubleProperty();
 
-        System.out.println("[empty] " + empty + "  [item]" + item);
+//    private String name;
+//    private int age;
+//    private double score;
 
-        HBox hBox1 = new HBox(10);
-        hBox1.setAlignment(Pos.CENTER);
-//        hBox1.setMaxWidth(220);
-//        hBox1.setMaxHeight(220);
-        hBox1.setStyle("-fx-background-color: #FF82AB");
-        hBox1.getChildren().addAll(new Button(item), new Button(item));
+    public Student(String name, int age, double score) {
+        this.name.setValue(name);
+        this.age.setValue(age);
+        this.score.setValue(score);
 
-        this.setStyle("-fx-background-color: #B0E0E6");
-        this.setGraphic(hBox1);
-//        this.setPrefWidth(350);
-//        this.setPrefHeight(350);
-        this.setAlignment(Pos.CENTER);
-        this.setContentDisplay(ContentDisplay.RIGHT);
+//        this.name = name;
+//        this.age = age;
+//        this.score = score;
+    }
+
+
+    public String getName() {
+        return name.getValue();
+//        return name;
+    }
+
+    public void setName(String name) {
+        this.name.setValue(name);
+//        this.name = name;
+    }
+
+    public int getAge() {
+        return age.getValue();
+//        return age;
+    }
+
+    public void setAge(int age) {
+        this.age.setValue(age);
+//        this.age = age;
+    }
+
+    public double getScore() {
+        return score.getValue();
+//        return score;
+    }
+
+    public void setScore(double score) {
+        this.score.setValue(score);
+//        this.score = score;
+    }
+
+    public SimpleStringProperty getNameSimpleStringProperty(){
+        return name;
     }
 }
