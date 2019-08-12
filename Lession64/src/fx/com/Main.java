5adfebd9a81020e98f79c2b44f79e92ba26d0321
Miko <MikoSecSoS@gmail.com>
2019-08-12 20:59:58 +0800

    commit

--- Lession64/src/fx/com/Main.java
+++ Lession64/src/fx/com/Main.java
@@ -1,15 +1,28 @@
 package fx.com;
 
 import javafx.beans.Observable;
+import javafx.beans.property.SimpleListProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.util.Callback;
 
-public class Main1 {
+public class Main {
 
     public static void main(String[] args) {
+//        ObservableList<String> list = FXCollections.observableList();
+//
+//        list.add("Miko");
+//        list.add("Code");
+//        list.add("AI");
+//        list.add("Cute");
+//        list.add("qwq");
+//
+//        FXCollections.rotate(list, 2);
+//
+//        list.forEach(System.out::println);
+
         SimpleStringProperty simpleStringProperty = new SimpleStringProperty("Miko");
         SimpleStringProperty simpleStringProperty1 = new SimpleStringProperty("Code");
 
@@ -18,7 +31,8 @@ public class Main1 {
             public Observable[] call(SimpleStringProperty param) {
                 System.out.println("[call] " + param);
 
-                Observable[] observables = new Observable[] {param};
+//                Observable[] observables = new Observable[] {param};
+                Observable[] observables = new Observable[] {new SimpleStringProperty("qwq")};
 
                 return observables;
             }
@@ -35,10 +49,22 @@ public class Main1 {
             }
         });
 
-        ObservableList<SimpleStringProperty> list1 = FXCollections.observableList(list, new Callback<SimpleStringProperty, Observable[]>() {
+//        list.add(simpleStringProperty);
+
+//        simpleStringProperty.set("Miko~~~");
+
+//        list.add(simpleStringProperty);
+
+//        list.forEach(System.out::println);
+
+//        System.out.println(list.get(0).get());
+
+        SimpleStringProperty simpleStringProperty2 = new SimpleStringProperty("AI");
+
+        ObservableList<SimpleStringProperty> list1 = FXCollections.observableArrayList(new Callback<SimpleStringProperty, Observable[]>() {
             @Override
             public Observable[] call(SimpleStringProperty param) {
-                System.out.println("[call1] " + param);
+                System.out.println("[call] " + param);
 
                 Observable[] observables = new Observable[] {param};
 
@@ -46,27 +72,22 @@ public class Main1 {
             }
         });
 
-        list.addListener(new ListChangeListener<SimpleStringProperty>() {
+        SimpleListProperty<SimpleStringProperty> simpleListProperty = new SimpleListProperty<SimpleStringProperty>(list1);
+
+        simpleListProperty.addListener(new ListChangeListener<SimpleStringProperty>() {
             @Override
             public void onChanged(Change<? extends SimpleStringProperty> c) {
 
                 while (c.next()) {
-                    System.out.println("[onChanged1] " + c + " [wasUpdated1] " + c.wasUpdated());
+                    System.out.println("[onChanged] " + c + " [wasUpdated] " + c.wasUpdated());
                 }
 
             }
         });
 
-        list.add(simpleStringProperty);
-        list1.add(simpleStringProperty);
+        simpleListProperty.add(simpleStringProperty2);
 
-        list.forEach(item -> System.out.println("[list] " + item.get()));
-        list1.forEach(item -> System.out.println("[list1] " + item.get()));
-
-        simpleStringProperty.set("AI");
-
-        list.forEach(item -> System.out.println("[list] " + item.get()));
-        list1.forEach(item -> System.out.println("[list1] " + item.get()));
+        simpleStringProperty2.set("Cute");
 
     }
 
